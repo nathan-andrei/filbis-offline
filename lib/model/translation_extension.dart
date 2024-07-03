8f5c5402449cbe7f9d34684555c62949cf11d4f2
wailingwailord <maximusburce@gmail.com>
2024-07-03 14:37:45 +0800

    Create translation_extension.dart

---
+++ lib/model/translation_extension.dart
@@ -0,0 +1,31 @@
+import 'collections.dart';
+
+extension QuestionTranslationExtension on QuestionTranslation {
+  String? getTranslation(String language) {
+    switch (language) {
+      case 'filipino':
+        return tagalogResponse;
+      case 'english':
+        return englishResponse;
+      case 'cebuano':
+        return cebuanoResponse;
+      default:
+        return tagalogResponse;
+    }
+  }
+}
+
+extension QckReplyExtension on QckReply {
+  List<String>? getTranslation(String language) {
+    switch (language) {
+      case 'filipino':
+        return tagalogReplies;
+      case 'english':
+        return englishReplies;
+      case 'cebuano':
+        return cebuanoReplies;
+      default:
+        return tagalogReplies;
+    }
+  }
+}
