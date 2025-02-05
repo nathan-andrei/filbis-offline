b2c1c7bd17b569b898aa398e6f95255f7634b8ca
nathan-andrei <nathan_andrei_asnan@dlsu.edu.ph>
2025-02-05 16:54:51 +0800

    Cleaned up and fixed state setting with currChoice

--- lib/widgets/lang_dropdown.dart
+++ lib/widgets/lang_dropdown.dart
@@ -1,55 +1,30 @@
-import 'package:filbis_offline/widgets/button.dart';
 import 'package:filbis_offline/model/collections_controller.dart';
-import 'package:filbis_offline/widgets/textinput.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:auto_size_text/auto_size_text.dart';
-
-/*
-class ModulePage extends StatefulWidget {
-  final ValueChanged<String> onButtonPressed;
-
-  const ModulePage({required this.onButtonPressed, super.key});
-
-  @override
-  State<ModulePage> createState() => _ModuleState();
-}*/
-
-/*
-class DropDownButtonApp extends StatelessWidget{
-  const DropDownButtonApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(title: const Text('Choose Language')),
-        body: const Center(
-          child: DropdownButton
-        ),
-      )
-    );
-  }
-}*/
-
 
 // https://api.flutter.dev/flutter/material/DropdownButton-class.html FOR REFERENCE
-class LanguageDropDown extends StatefulWidget{
-  String currChoice;
 
-  //currChoice is passed by the homepage so we can tell the dropdown
+class LanguageDropDown extends StatefulWidget{
+  final String passedChoice;
+
+  //passedChoice is passed by the homepage so we can tell the dropdown
   //what was the first language picked
-  LanguageDropDown({required this.currChoice, super.key});
+  const LanguageDropDown({required this.passedChoice, super.key});
 
   @override
   State<LanguageDropDown> createState() => _LanguageDropDownState();
-
-  set CurrChoice(String val) => currChoice = val;
 }
 
 const List<String> langChoices = ["Filipino", "English", "Cebuano"];
 
 class _LanguageDropDownState extends State<LanguageDropDown>{
+  late String currChoice;
+
+  @override
+  initState(){
+    currChoice = widget.passedChoice;
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context){
@@ -59,13 +34,13 @@ class _LanguageDropDownState extends State<LanguageDropDown>{
       ),
       child: DropdownButton<String>( //Try different vals
         dropdownColor: const Color(0xffed7042),
-        value: widget.currChoice[0].toUpperCase() + widget.currChoice.substring(1),
+        value: currChoice,
         icon: const Icon(Icons.arrow_downward, color: Colors.white),
         elevation: 16,
         style: const TextStyle(color: Colors.white),
         onChanged: (String? value){
           setState(() {
-            widget.currChoice = value!;
+            currChoice = value!;
           });
           context.read<FilbisDatabase>().setLanguage(value!.toLowerCase(), false);
         },
